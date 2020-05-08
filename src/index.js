166935c3f3a17dc3f0f2b349c34801ff1a89455c
Andrew <andch763@gmail.com>
2020-05-08 20:34:57 +0300

    Update index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import SampleSearch from "./search/SearchBox";
+import SampleSearch from "./containers/SearchBox";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
